c73fc0156e196cab9204e2222bed00bb89137fe5
555f <555f@mail.com>
2024-05-21 18:31:48 +0300

    feat: added context

---
+++ client_test.go
@@ -0,0 +1,9 @@
+package jsonrpc_test
+
+import (
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+
+}
---
+++ server_test.go
@@ -0,0 +1,9 @@
+package jsonrpc_test
+
+import (
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+
+}
